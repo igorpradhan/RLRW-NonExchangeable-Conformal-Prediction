c0cc3b1bc47b16a3522e8902cdcfd2581f61a9fd
Igor Pradhan <pradhanigor@gmail.com>
2024-05-20 18:20:15 +0200

    finalised methods

--- utils/methods.py
+++ utils/methods.py
@@ -73,7 +73,6 @@ class METHODS():
         else:
             quantile = float('-inf')
             closed = False
-
         return quantile, closed
         
 
@@ -110,3 +109,53 @@ class METHODS():
             i += 1
         
         return h_opt
+
+        
+    """
+        Locally weighted CP methods
+    """
+    
+    def RLCP(X_calib, scores_calib, X_test, scores_test, kernel, h, alpha):
+        n_test, d = Xtest.shape
+        coverage = score_threshold = [0] * n_test
+        
+        Xcalib = Xcalib[np.argsort(scores_calib), :]
+        scores_calib = np.sort(scores_calib)        
+        
+        scores = (scores, np.inf)
+        indices = []
+        j = i = 1
+        scores_unique = np.array()
+        while i < len(scores):
+                current_score = scores[i]
+                scores_unique.append(current_score)
+                current_indices = np.where(scores == current_score)[0]
+                indices.append(current_indices)
+                i += len(current_indices)
+            
+        for i in range(n_test):
+            xtest = X_test[i]
+            test_score = scores_test[i]
+            cov_data = np.vstack([Xcalib, xtest])
+            
+            # Finding the weights and the score threshold
+            if kernel == "gaussian":
+                xtilde_test = multivariate_normal.rvs(mean=xtest, cov=np.diag(np.ones(d)) * h**2)
+                weights = multivariate_normal.pdf(cov_data, mean=xtilde_test, cov=np.diag(np.ones(d)) * h**2)
+                result = METHODS.smoothed_weighted_quantile(scores_unique, alpha, weights, indices)
+            elif kernel == "box":
+                xtilde_test = METHODS.runifball(1, xtest, h).flatten()
+                weights = np.array([1 if euclid_distance(x, xtilde_test) <= h else 0 for x in cov_data])
+                result = METHODS.smoothed_weighted_quantile(scores_unique, alpha, weights, indices)
+            
+            score_threshold[i] = result[0]  # score_threshold
+            closed = result[1]   # whether it's a closed interval
+            
+            # Coverage
+            coverage[i] = int(test_score < score_threshold[i])
+            if closed:
+                coverage[i] = int(test_score <= score_threshold[i])
+        
+        return np.column_stack((coverage, score_threshold))
+
+    
